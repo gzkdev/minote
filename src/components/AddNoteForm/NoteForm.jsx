e400781cb6766a15c3dbe5334825372ef8bd9915
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-15 03:19:31 +0100

    improved addnoteform ui

--- src/components/AddNoteForm/NoteForm.jsx
+++ src/components/AddNoteForm/NoteForm.jsx
@@ -10,24 +10,26 @@ function NoteForm({ isOpen, setIsOpen }) {
   const noteText = useRef();
 
   const handleAddNote = () => {
-    let date = new Date();
-    let data = {
-      id: date.toISOString(),
-      title: noteTitle.current.value,
-      text: noteText.current.value,
-      date: date.toLocaleDateString(),
-    };
-    addNote(data);
-    setIsOpen(!isOpen);
-    noteTitle.current.value = noteText.current.value = "";
+    if (noteTitle.current.value) {
+      let date = new Date();
+      let data = {
+        id: date.toISOString(),
+        title: noteTitle.current.value,
+        text: noteText.current.value,
+        date: date.toLocaleDateString(),
+      };
+      addNote(data);
+      setIsOpen(!isOpen);
+      noteTitle.current.value = noteText.current.value = "";
+    }
   };
 
   return (
     <NoteFormStyled as={motion.div} layoutId="form">
       <div className="wrapper">
         <div className="top">
-          <button>
-            <LineIcon name="times" />
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <LineIcon name="close" />
           </button>
           <button onClick={handleAddNote}>Save</button>
         </div>
