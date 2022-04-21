a674e429843f053d4267d190d101c958931088e9
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-21 13:52:51 +0100

    updated globalstyles

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,43 +1,8 @@
 import { useContext } from "react";
 import NotesContext from "../../NotesContext";
-import styled from "styled-components";
 import SearchInput from "./SearchInput";
 import LineIcon from "react-lineicons";
-
-const SearchBarStyled = styled.div`
-  position: fixed;
-  top: 1rem;
-  left: 50%;
-  transform: translate(-50%, 0);
-  display: flex;
-  align-items: center;
-  width: 95%;
-  max-width: 1000px;
-  min-height: 64px;
-  padding: 0 1rem;
-  background-color: var(--color-100);
-  border: 1px solid var(--color-800);
-  z-index: 900;
-  border-radius: 8px;
-
-  & input {
-    flex-grow: 1;
-  }
-
-  & button {
-    width: 40px;
-    font-size: 1.25rem;
-    margin-left: 1rem;
-    border-radius: 4px;
-    cursor: pointer;
-    transition: transform 200ms, background-color 100ms;
-
-    &:active {
-      transform: scale(0.9);
-      background-color: var(--color-off-white);
-    }
-  }
-`;
+import SearchBarStyled from "./SearchBar.styled";
 
 function SearchBar() {
   const { searchText, setSearchText, toggleTheme } = useContext(NotesContext);
