ae71c72e9b7fdba092939a9c0c8804ef189a92a2
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-14 16:03:03 +0100

    created styled folder for styled components

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -1,40 +1,6 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 import EmptyStateImage from "../assets/images/empty-state.svg";
-
-const EmptyStateStyled = styled.div`
-  max-width: 90%;
-  min-height: 80vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-  text-align: center;
-
-  & img {
-    display: block;
-    width: 164px;
-    height: 164px;
-    margin-bottom: 2rem;
-    object-fit: contain;
-    pointer-events: none;
-  }
-
-  & a {
-    min-height: 48px;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: var(--color-100);
-    background-color: var(--color-300);
-    padding: 0 2rem;
-    margin-top: 2rem;
-    font-weight: 600;
-    border-radius: 4px;
-  }
-`;
+import { EmptyStateStyled } from "./styled";
 
 function EmptyState() {
   return (
