05864a149f291c38ded063bb1839a154d395cecc
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-14 18:28:33 +0100

    updated notes container styled components

--- src/components/Notes/NotesContainer.jsx
+++ src/components/Notes/NotesContainer.jsx
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 const NotesContainer = styled.div`
   --grid-min-width: 240px;
-  width: min(90%, 800px);
+  width: min(100%, 800px);
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(var(--grid-min-width), 1fr));
   gap: 1rem;
-  padding-bottom: 8rem;
+  padding: 2rem 1rem 8rem;
   margin: 0 auto;
 
   @media (min-width: 768px) {
