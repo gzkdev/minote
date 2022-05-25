ae85c25e7a55c566510f247e5bc81fb4468ece23
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-25 18:12:26 +0100

    added index pages file for managed exports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { NotesProvider } from "./NotesContext";
 import GlobalStyles from "./components/GlobalStyles";
 import Menu from "./components/Menu";
-import Home from "./Pages/Home";
-import Favorites from "./Pages/Favorites";
-import Trash from "./Pages/Trash";
+import { Home, Favorites, Trash, AddNote } from "./Pages";
 import AppContainer from "./components/styled/AppContainer.styled";
 
 function App() {
@@ -16,6 +14,7 @@ function App() {
           <Menu />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/add-note" element={<AddNote />} />
             <Route path="/favorites" element={<Favorites />} />
             <Route path="/trash" element={<Trash />} />
           </Routes>
---
+++ src/Pages/index.js
@@ -0,0 +1,7 @@
+import AddNote from "./AddNote";
+import Favorites from "./Favorites";
+import Home from "./Home";
+import Note from "./Note";
+import Trash from "./Trash";
+
+export { AddNote, Favorites, Home, Trash, Note }
